44c4c9b8fb2a727e1bbca996d6b08988c48642d7
Shalaka <shalakav17@gmail.com>
2023-06-09 21:29:35 +0530

    switch.......

--- switch statement.cpp
+++ switch statement.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-// int num = 1;
+int num = 1;
 
-// int main() {
-//   switch (num) {
-//   case 1:
-//     cout << "First" << endl;
-//       break;
-//   case 2:
-//     cout << "Second" << endl;
-//       break;
-//   }
-// }
+int main() {
+  switch (num) {
+  case 1:
+    cout << "First" << endl;
+      break;
+  case 2:
+    cout << "Second" << endl;
+      break;
+  }
+}
 
 // Switch has flow such that if any case matches, then this case's block come
 // into flow and if the break statement is not there to indicate the end of this
@@ -19,41 +19,41 @@ using namespace std;
 // So, as you can see there seems to be no use of continue here.//
 
 
-//******************Calculator code***********************
-int a, b;
-char c;
-int result;
+// ******************Calculator code***********************
+// int a, b;
+// char c;
+// int result;
 
-int main() {
-  cout << "Enter value of a:";
-  cin >> a;
-  cout << "Enter value of b:";
-  cin >> b;
-  cout << "Enter opertaor:";
-  cin >> c;
+// int main() {
+//   cout << "Enter value of a:";
+//   cin >> a;
+//   cout << "Enter value of b:";
+//   cin >> b;
+//   cout << "Enter opertaor:";
+//   cin >> c;
 
-  switch (c) {
-  case '*':
-    result = a * b;
-    cout << "result=" << result;
-    break;
-  case '+':
-    result = a + b;
-    cout << "result=" << result;
-    break;
-  case '/':
-    result = a / b;
-    cout << "result=" << result;
-    break;
-  case '-':
-    result = a - b;
-    cout << "result=" << result;
-    break;
-  case '%':
-    result=a%b;
-    cout << "result=" << result;
-    break;
-  default:
-    cout<<"Exit"<<endl;
-  }
-}
+//   switch (c) {
+//   case '*':
+//     result = a * b;
+//     cout << "result=" << result;
+//     break;
+//   case '+':
+//     result = a + b;
+//     cout << "result=" << result;
+//     break;
+//   case '/':
+//     result = a / b;
+//     cout << "result=" << result;
+//     break;
+//   case '-':
+//     result = a - b;
+//     cout << "result=" << result;
+//     break;
+//   case '%':
+//     result=a%b;
+//     cout << "result=" << result;
+//     break;
+//   default:
+//     cout<<"Exit"<<endl;
+//   }
+// }
