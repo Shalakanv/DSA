9d59b11dd68c093c82656d423e5702d6176e897b
Shalaka <shalakav17@gmail.com>
2023-06-12 14:03:18 +0530

    Problems

--- functions.cpp
+++ functions.cpp
@@ -25,31 +25,60 @@ using namespace std;
 
 // }
 
-bool isEven(int a)
+// bool isEven(int a)
+// {
+//   if(a%2==0)
+//   {
+//     return true;
+//   }
+//   else
+//   {
+//     return false;
+//   }
+// }
+
+// int main()
+// {
+//   int num;
+//   cout<<"Enter number which you want to check:";
+//   cin>>num;
+
+//   if(isEven(num))
+//   {
+//     cout<<"Number is even";
+//   }
+//   else
+//   {
+//     cout<<"Number is odd";
+//   }
+  
+// }
+
+
+int factorial(int n)   // function signature
 {
-  if(a%2==0)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+  // function body
+  int ans=1;
+  for(int i=1;i<=n;i++)
+    {
+      ans=ans*i;
+    }
+  return ans;
+}
+
+int nCr(int n,int r)
+{
+  int result;
+  result=(factorial(n))/(factorial(r)*factorial(n-r));
+  return result;
 }
 
 int main()
 {
-  int num;
-  cout<<"Enter number which you want to check:";
-  cin>>num;
-
-  if(isEven(num))
-  {
-    cout<<"Number is even";
-  }
-  else
-  {
-    cout<<"Number is odd";
-  }
-  
+  int n,r;
+  cout<<"enter value for n:";
+  cin>>n;
+  cout<<"enter value for r:";
+  cin>>r;
+  cout<<"Result is:"<<nCr(n,r); 
 }
