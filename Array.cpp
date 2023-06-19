81296d82a24e7f6c4917c43c205361f86fc1814e
Shalaka <shalakav17@gmail.com>
2023-06-19 19:18:29 +0530

    Use of function in array

--- Array.cpp
+++ Array.cpp
@@ -15,11 +15,29 @@ using namespace std;
 //   i++;
 // }
 
+void print_array(int array1[],int size)
+{
+  cout<<"Printing array"<<endl;
+  for(int i=0;i<size;i++)
+    {
+      cout<<array1[i]<<" "<<endl;
+    }
+  cout<<"Printing done"<<endl;
+  return;
+}
+
+int main() 
+{
+  int array1[5]={1,2,3,4,5};
+  print_array(array1,5);
+  cout<<"Value at 4th index is "<<array1[4];
+}
+
 //To find length of array
 
-int main()
-{
-  int arr[5]={11,12,13,14,15};
-  int length=sizeof(arr)/sizeof(arr[0]);
-  cout<<"Length of array is "<<length;
-}
+// int main()
+// {
+//   int arr[5]={11,12,13,14,15};
+//   int length=sizeof(arr)/sizeof(arr[0]);
+//   cout<<"Length of array is "<<length;
+// }
