4ae1cf406ca2b81fdb8e5513b7c16a8e8f0e7484
Shalaka <shalakav17@gmail.com>
2023-06-23 08:48:20 +0530

    updation of value in array

---
+++ updation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void update(int arra[],int n)
+{
+   arra[0]=50;
+  cout<<"Before updation"<<endl;
+  for(int i=0;i<n;i++)
+    {
+      cout<<arra[i]<<" ";
+    }
+  cout<<"\nAfter updation"<<endl;
+}
+
+int main()
+{
+  int arra[4]={0,10,20,30};
+  update(arra,4); // In update function we are not actually passing the whole array,we are passing the address of the first location  
+
+  for(int i=0;i<4;i++)
+    {
+      cout<<arra[i]<<" ";
+    }
+}
