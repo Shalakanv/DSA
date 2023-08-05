51f5fbd8aa333a41b92b84ba63dc383cdb8f1abe
Shalaka Vyawhare <131943469+Shala-Bela@users.noreply.github.com>
2023-08-05 06:24:13 -0700

    Add files via upload

---
+++ sumpair.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int arr[]={1,2,3,4,5};
+  int ans1,ans2;
+  int sum=5;
+
+  for(int i=0;i<5;i++)
+    {
+      for(int j=i+1;j<5;j++)
+        {
+          if(arr[i]+arr[j]==sum)
+          {
+            int temp=min(arr[i],arr[j]);
+            ans1=max(arr[i],arr[j]);
+            ans2=temp;
+            cout<<ans1<<" "<<ans2<<" "<<endl;
+          }
+        }
+    }
+}
